e8c404683345e86451e96ef76e5f0de723c4362c
Sanket Hiremath <73591040+Sanket01Hiremath@users.noreply.github.com>
2022-11-12 00:38:28 +0530

    User fuctions are added

--- Sulabh Bank/src/com/DBInterface/DBInterface.java
+++ Sulabh Bank/src/com/DBInterface/DBInterface.java
@@ -2,13 +2,21 @@ package com.DBInterface;
 
 import com.Beans.UserBean;
 
+import java.sql.ResultSet;
+
 public interface DBInterface {
     public Boolean ConnectToDB(String username,String password);
-    public Boolean ConnectUserToDB(String username,String password);
+    public void ConnectUserToDB(String username,String password);
     public String CreateUserBean();
     public String CreateUser(UserBean userBean);
+    public String CreateDepositTable(int name);
+    public String CreateTransferTable(int name);
     public String EditUserData();
     public String RemoveUser();
     public String ShowSpecificUserData();
     public String ShowAllUsersData();
+    public String ShowTransactions(int AcNo);
+    public String AddMoney(int AcNo);
+    public String TransferMoney(int AcNo);
+
 }
--- Sulabh Bank/src/com/UserTasks/UserTasksMenu.java
+++ Sulabh Bank/src/com/UserTasks/UserTasksMenu.java
@@ -1,42 +1,35 @@
-package com.AdminTasks;
+package com.UserTasks;
 
 import Home.FirstPage;
+import com.AdminTasks.AdminTaskMenu;
 import com.DBInterface.DBInterface;
 import com.DBInterface.DBInterfaceImpl;
+
 import java.util.Scanner;
 
-public class AdminTaskMenu {
-    public static void ChooseATask(){
+public class UserTasksMenu {
+    public static void ChooseATask(int AcNo){
         Scanner sc=new Scanner(System.in);
         System.out.println("-------------------------------------------------------------------");
-        System.out.println("                    Important Note:\ntype 1: To add new Customer,\ntype 2: To Update Customer Data,\ntype 3: To Remove Customer,\ntype 4: To Get A Customer Details,\ntype 5: To Get All Customers Details,\ntype 8: To LogOut");
+        System.out.println("                    Important Note:\ntype 1: To Add Money,\ntype 2: To Transfer Money ,\ntype 3: To Check Balance,\ntype 4: To Get Transaction History,\ntype 8: To LogOut");
         System.out.print("Choose a task you want to perform: ");
         int i=sc.nextInt();
         System.out.println("-------------------------------------------------------------------");
         DBInterface DBI=new DBInterfaceImpl();
 
         if(i==1){
-            System.out.println(DBI.CreateUserBean());
-            AdminTaskMenu.ChooseATask();
+            System.out.println(DBI.AddMoney(AcNo));
+            UserTasksMenu.ChooseATask(AcNo);
         }else if(i==2){
-            System.out.println(DBI.EditUserData());
-            AdminTaskMenu.ChooseATask();
-        }else if(i==3){
-            System.out.println(DBI.RemoveUser());
-            AdminTaskMenu.ChooseATask();
-        }else if(i==4){
-            System.out.println(DBI.ShowSpecificUserData());
-            AdminTaskMenu.ChooseATask();
-        }else if(i==5){
-            System.out.println(DBI.ShowAllUsersData());
-            AdminTaskMenu.ChooseATask();
+            System.out.println(DBI.TransferMoney(AcNo));
+            UserTasksMenu.ChooseATask(AcNo);
         }else if(i==8){
             System.out.println("-------------------------------------------------------------------");
             System.out.println("                          LogOut Successful                        ");
             System.out.println("-------------------------------------------------------------------");
             FirstPage.firstPage();
         }else{
-            AdminTaskMenu.ChooseATask();
+            UserTasksMenu.ChooseATask(AcNo);
         }
     }
 }
